8246276b0779806ba904c498a055b48b278d59aa
Himanshu <himanshus@newgenit.com>
2024-07-20 17:24:14 +0530

    data

--- src/app.js
+++ src/app.js
@@ -16,3 +16,18 @@ app.use(express.urlencoded({ limit: "20kb" })); // make the data coming from URL
 app.use(cookieParser());
 
 export default app;
+
+/*
+env for github
+PORT=9000
+MONGODB_URL=mongodb+srv://himanshu:Him%4022112001@cluster0.e8ev2p4.mongodb.net
+CORS_ORIGIN=*
+ACESS_TOKEN_SECRET=You-CAN-Write-any-thing-Here_BUT-reCommEnDED_is-to-useGeneratED_TOKeN
+ACESS_TOKEN_EXPIRY=1d
+REFRESH_TOKEN_SECRET=You-caN_WRite-ANYthINg_here_AlSo
+REFRESH_TOKEN_EXPIRY=10d
+
+CLOUDINARY_CLOUD_NAME=del26rnuj
+CLOUDINARY_API_KEY=173346169481458
+CLOUDINARY_API_SECRET=YDE8LIDsbfrGqEkc9Nx4Es_hpSM
+*/
