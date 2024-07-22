3b68b5d5060cbbd0cecf9f9db6915f85682bbba6
Himanshu <himanshus@newgenit.com>
2024-07-22 14:53:02 +0530

    controllers and routes

--- src/app.js
+++ src/app.js
@@ -15,19 +15,8 @@ app.use(express.json({ limit: "20kb" })); //limit the form data
 app.use(express.urlencoded({ limit: "20kb" })); // make the data coming from URL in a same format
 app.use(cookieParser());
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export default app;
-
-/*
-env for github
-PORT=9000
-MONGODB_URL=mongodb+srv://himanshu:Him%4022112001@cluster0.e8ev2p4.mongodb.net
-CORS_ORIGIN=*
-ACESS_TOKEN_SECRET=You-CAN-Write-any-thing-Here_BUT-reCommEnDED_is-to-useGeneratED_TOKeN
-ACESS_TOKEN_EXPIRY=1d
-REFRESH_TOKEN_SECRET=You-caN_WRite-ANYthINg_here_AlSo
-REFRESH_TOKEN_EXPIRY=10d
-
-CLOUDINARY_CLOUD_NAME=del26rnuj
-CLOUDINARY_API_KEY=173346169481458
-CLOUDINARY_API_SECRET=YDE8LIDsbfrGqEkc9Nx4Es_hpSM
-*/
