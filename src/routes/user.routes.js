400e6dc0bbe8cb476a7977321e86d6e35ed1dad9
Himanshu Sharma <himanshuhssharma9@gmail.com>
2024-10-25 08:56:34 +0530

    refresh tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
@@ -24,6 +25,9 @@ userRouter.route("/register").post(
 );
 
 userRouter.route("/login").post(loginUser);
+
+//secure route
 userRouter.route("/logout").post(verifyJWT, logoutUser);
+userRouter.route("/refresh-token").post(refreshAccessToken);
 
 export default userRouter;
